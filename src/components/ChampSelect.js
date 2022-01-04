b47b79d191e0c4175870b5fa34c3806802fec82b
Tom <tommusique974@gmail.com>
2022-01-04 17:41:02 +0100

    feat: add champselect bg

--- src/components/ChampSelect.js
+++ src/components/ChampSelect.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Text, Box} from '@chakra-ui/react'
+import ChampSelectBG from '../assets/champselect.png'
 
 const ChampSelect = () => {
 	return (
-		<Box w='100%' h='600px' bg='green' mt='100px' p='5'>
+		<Box w='100%' h='1200px' backgroundImage={ChampSelectBG} backgroundRepeat='no-repeat' mt='100px' p='5'>
 			<Text variant='TitleCenter'>Choose your champion</Text>
 		</Box>
 	);
