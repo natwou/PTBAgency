40fcc8369ee146802d7496262983428b992467da
Tom <tommusique974@gmail.com>
2022-01-04 17:54:37 +0100

    ok

--- src/components/ChampSelect.js
+++ src/components/ChampSelect.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text, Box} from '@chakra-ui/react'
+import {Text, Box, Image} from '@chakra-ui/react'
 import ChampSelectBG from '../assets/champselect.png'
 
 const ChampSelect = () => {
 	return (
-		<Box w='100%' h='1200px' backgroundImage={ChampSelectBG} backgroundRepeat='no-repeat' mt='100px' p='5'>
+		<Box w='100%' h='auto' mt='100px' p='5'>
 			<Text variant='TitleCenter'>Choose your champion</Text>
+			<Image src={ChampSelectBG} w="1500px" h='auto' d='block' margin='20px auto 0'/>
 		</Box>
 	);
 };
